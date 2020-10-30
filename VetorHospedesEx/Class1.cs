c5ce936e02a6b115e5cb689b54f50fcf262166a0
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-29 22:59:36 -0300

    pronto

---
+++ VetorHospedesEx/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetorHospedesEx
+{
+    class Hospedes
+    {
+        public string Nome { get; set; } //Get e Set dos Nomes dos hóspedes
+        public string Email { get; set; } //Get e Set dos Emails dos Hóspedes
+        public Hospedes(string nome, string email) //Construtor personalizado
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString() //Declaração da String
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
--- VetorHospedesEx/Program.cs
+++ VetorHospedesEx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace VetorHospedesEx
 {
@@ -6,7 +7,32 @@ namespace VetorHospedesEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hospedes[] vect = new Hospedes[10];
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel # {i}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quartos: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Hospedes(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
         }
     }
 }
