f39827f524608a7f902b385f031e9603138d9850
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-30 17:25:13 -0300

    pronto

--- VetorHospedesEx/Program.cs
+++ VetorHospedesEx/Program.cs
@@ -8,13 +8,13 @@ namespace VetorHospedesEx
         static void Main(string[] args)
         {
             Hospedes[] vect = new Hospedes[10];
-            Console.WriteLine("Quantos quartos serão alugados? ");
+            Console.Write("Quantos quartos serão alugados? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
                 Console.WriteLine();
-                Console.WriteLine($"Aluguel # {i}: ");
+                Console.WriteLine($"Aluguel #{i+1}: ");
                 Console.Write("Nome: ");
                 string nome = Console.ReadLine();
                 Console.Write("E-mail: ");
@@ -33,6 +33,7 @@ namespace VetorHospedesEx
                     Console.WriteLine(i + ": " + vect[i]);
                 }
             }
+            Console.ReadKey();
         }
     }
 }
