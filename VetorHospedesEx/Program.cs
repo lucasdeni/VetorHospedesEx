124b9cdecfbe70dfe14c79eb9d11fb1a53d50ac4
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-29 22:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ VetorHospedesEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VetorHospedesEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
